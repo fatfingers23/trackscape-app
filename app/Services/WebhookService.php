e0c0ea73766183ea933d57e2533aa4c88b5ecd26
Bailey Townsend <baileytownsend2323@gmail.com>
2023-01-03 21:04:32 -0600

    New embedded messages

--- app/Services/WebhookService.php
+++ app/Services/WebhookService.php
@@ -4,29 +4,49 @@ namespace App\Services;
 
 use App\Models\ChatLog;
 use App\Models\Clan;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
 
 class WebhookService
 {
 
-    public function sendSimpleMessage(Clan $clan, ChatLog $chatLog)
+    public function sendSimpleMessage(Clan $clan, ChatLog $chatLog, string $rank = null)
     {
         $webHookUrl = $clan->discord_webhook;
         if ($webHookUrl == null) {
             return;
         }
 
+        $rankLink = "";
+        if($rank){
+            $upperCaseRank = ucfirst($rank);
+            $rankLink = "https://wiseoldman.net/img/runescape/roles/$upperCaseRank.png";
+        }
+        else if($clan->name == $chatLog->sender){
+            $rankLink = "https://oldschool.runescape.wiki/images/Your_Clan_icon.png";
+        }
+        else{
+            $rankLink = "https://oldschool.runescape.wiki/images/Clan_icon_-_Guest.png";
+        }
+
         $body = [
             "username" => $clan->name,
+            //Can you translate the bottom from json to a php array?
             "embeds" => [
                 [
+                    "title" => "",
+                    "description" => $chatLog->message,
+                    "color" => 0x00FFFF,
+                    "timestamp" => Carbon::now('UTC')->toIso8601String(),
                     "author" => [
-                        "name" => $chatLog->sender
-                    ],
-                    "description" => $chatLog->message
+                        "name" => $chatLog->sender,
+                        'icon_url' => $rankLink
+                    ]
                 ]
             ]
         ];
+
+
         Http::post($webHookUrl, $body);
     }
 
